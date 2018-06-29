dcd807c3faa8ae00b9327995f938e53f2f49bdcd
narisuke0403 <sungwoop0403@yahoo.co.jp>
2018-06-29 17:59:12 +0900

    訂正

--- analyze/hierarchyPlace.py
+++ analyze/hierarchyPlace.py
@@ -1,39 +1,31 @@
 import os,csv,copy,shutil,concurrent.futures,numpy as np,math
 x_li = []
 y_li = []
-All_Node_x = []
-All_Node_y = []
-x1 = []
-x2 = []
-y1 = []
-y2 = []
+All_Node = []
 
-class Node_x:
-  def __init__(self,_min,_max,_rank):
-    self.min = _min
-    self.max = _max
+class Node:
+  def __init__(self,_minA,_maxA,_minR,_maxR,_rank):
+    self.minA = _minA
+    self.maxA = _maxA
+    self.minR = _minR
+    self.maxR = _maxR
     self.rank = _rank
     self.child1 = None
     self.child2 = None
+    self.child3 = None
+    self.child4 = None
     self.haschiled = False
     self.count = 0
   
   def createchildren(self):
-    self.child1 = Node_x(self.min, self.min + (self.max-self.min)/2, self.rank+1)
-    self.child2 = Node_x(self.min + (self.max-self.min)/2, self.max , self.rank+1)
-    All_Node_x.append(self.child1)
-    All_Node_x.append(self.child2)
-    self.haschiled = True
-
-class Node_y(Node_x):
-  def __init__(self,_min,_max,_rank):
-    super().__init__(_min,_max,_rank)
-
-  def createchildren(self):
-    self.child1 = Node_y(self.min, self.min + (self.max-self.min)/2, self.rank+1)
-    self.child2 = Node_y(self.min + (self.max-self.min)/2, self.max , self.rank+1)
-    All_Node_y.append(self.child1)
-    All_Node_y.append(self.child2)
+    self.child3 = Node(self.minA, self.minA + (self.maxA-self.minA)/2,self.minR + (self.maxR-self.minR)/2,self.maxR,self.rank+1)
+    self.child2 = Node(self.minA + (self.maxA-self.minA)/2, self.maxA ,self.minR,self.minR + (self.maxR-self.minR)/2, self.rank+1)
+    self.child1 = Node(self.minA, self.minA + (self.maxA-self.minA)/2,self.minR,self.minR + (self.maxR-self.minR)/2, self.rank+1)
+    self.child4 = Node(self.minA + (self.maxA-self.minA)/2, self.maxA ,self.minR + (self.maxR-self.minR)/2,self.maxR, self.rank+1)
+    All_Node.append(self.child1)
+    All_Node.append(self.child2)
+    All_Node.append(self.child3)
+    All_Node.append(self.child4)
     self.haschiled = True
 
 def init():
@@ -52,12 +44,16 @@ def plot(data):
 def thresshold(x):
   return 3 ** x
 
-def search(x, node):
+def search(a,r, node):
   if node.haschiled:
-    if node.child1.min <= x and node.child1.max > x:
-      search(x, node.child1)
+    if node.child1.minA <= a and node.child1.maxA > a and node.child1.minR <= r and node.child1.maxR > r:
+      search(a,r,node.child1)
+    elif node.child2.minA <= a and node.child2.maxA > a and node.child2.minR <= r and node.child2.maxR > r:
+      search(a,r,node.child2)
+    elif node.child3.minA <= a and node.child3.maxA > a and node.child3.minR <= r and node.child3.maxR > r:
+      search(a,r,node.child3)
     else:
-      search(x, node.child2)
+      search(a,r,node.child4)
   else:
     node.count += 1
     if node.count > thresshold(node.rank):
@@ -66,20 +62,20 @@ def search(x, node):
   
 def main():
   plot(init())
-  root_x = Node_x(0,360,0)
-  root_y = Node_y(0,11,0)
+  root = Node(0,360,0,11,0)
   for i,_ in enumerate(x_li):
-    search(x_li[i],root_x)
-    search(y_li[i],root_y)
-  x1 = [All_Node_x[i].min for i in range(len(All_Node_x)) if All_Node_x[i].haschiled == False]
-  x2 = [All_Node_x[i].max for i in range(len(All_Node_x)) if All_Node_x[i].haschiled == False]
-  y1 = [All_Node_y[i].min for i in range(len(All_Node_y)) if All_Node_y[i].haschiled == False]
-  y2 = [All_Node_y[i].max for i in range(len(All_Node_y)) if All_Node_y[i].haschiled == False]
-  with open("out.csv","w+") as f:
-    writer = csv.writer(f,lineterminator="\n")
-    writer.writerow(x1)
-    writer.writerow(x2)
-    writer.writerow(y1)
-    writer.writerow(y2)
+    search(x_li[i],y_li[i],root)
+  A1 = [All_Node[i].minA for i in range(len(All_Node)) if All_Node[i].haschiled == False]
+  A2 = [All_Node[i].maxA for i in range(len(All_Node)) if All_Node[i].haschiled == False]
+  R1 = [All_Node[i].minR for i in range(len(All_Node)) if All_Node[i].haschiled == False]
+  R2 = [All_Node[i].maxR for i in range(len(All_Node)) if All_Node[i].haschiled == False]
+
+  with open("out.csv","w") as f:
+    writer = csv.writer(f,lineterminator='\n')
+    writer.writerow(A1)
+    writer.writerow(A2)
+    writer.writerow(R1)
+    writer.writerow(R2)
+
 if __name__ == '__main__':
     main()
